aae28d4cce1e45f8a1bb44f1a8828a8c04d0928c
Adriana Babakanian <ababakanian@berkeley.edu>
2018-05-15 16:26:53 -0700

    variable modularization

---
+++ src/Terminal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Terminal.css';
+
+const Terminal = () => (
+  <div className="Terminal">
+    <p> testing some content </p>
+  </div>
+);
+
+export default Terminal;
