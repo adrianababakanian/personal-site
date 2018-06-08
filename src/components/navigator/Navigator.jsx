3b775c61cc232007407e0ab4f67af4b1316cac1b
Adriana Babakanian <ababakanian@berkeley.edu>
2018-06-08 16:12:38 -0700

    change route with form

--- src/components/navigator/Navigator.jsx
+++ src/components/navigator/Navigator.jsx
@@ -1,42 +1,52 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import './Navigator.css';
 
-// class Navigator extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { value: ''};
-//
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-//
-//   handleChange(event) {
-//     this.setState({value: event.target.value});
-//   }
-//
-//   handleSubmit(event) {
-//     alert('A name was submitted: ' + this.state.value);
-//     event.preventDefault();
-//   }
-//
-//   render({history}) {
-//     return (
-//       <div>
-//         <button type='button' onClick={() => { history.push('/design')}}> to design we go! </button>
-//         <form onSubmit={this.handleSubmit} >
-//           <label>
-//             Name:
-//             <input type="text" value={this.state.value} onChange={this.handleChange}/>
-//           </label>
-//         </form>
-//       </div>
-//     )
-//   }
-// }
+class Navigator extends Component {
 
-const Navigator = withRouter(({ history }) => (
-  <button type='button' onClick={() => { history.push('/design')}}> to design we go! </button>
-))
+  static propTypes = {
+    match: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequried
+  }
 
-export default Navigator;
+  constructor(props) {
+    super(props);
+    this.state = { value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    const { match, location, history } = this.props;
+    history.push(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    const { match, location, history } = this.props;
+    return (
+      <div>
+        <button type='button' onClick={() => { history.push('/design')}}> to design we go! </button>
+        <form onSubmit={this.handleSubmit} >
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange}/>
+          </label>
+        </form>
+      </div>
+    )
+  }
+}
+
+// const Navigator = withRouter(({ history }) => (
+//   <button type='button' onClick={() => { history.push('/design')}}> to design we go! </button>
+// ))
+
+export default withRouter(Navigator);
