752a39314855013c91d0b9333940054e9bfe8562
Adriana Babakanian <ababakanian@berkeley.edu>
2018-06-08 15:42:51 -0700

    button navigator

--- src/components/content/home/Home.jsx
+++ src/components/content/home/Home.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import Terminal from './terminal/Terminal';
 import Footer from './../../footer/Footer';
+import Navigator from './../../navigator/Navigator';
 import './Home.css';
 
 class Home extends Component {
   render() {
     return (
       <div className="pageWrap homePage">
+        <Navigator />
         <Terminal colors={[
           "blue",
           "green",
           "orange"
         ]}/>
-        <Footer />
       </div>
     )
   }
